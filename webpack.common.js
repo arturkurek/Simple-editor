894353d060a7981bc5379a90818766ee8cbe0e82
Artur Kurek <arturkurek10@gmail.com>
2020-07-18 14:48:30 +0200

    Simple editor project - apperance + basic functions

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/text-editor.html",
+      inject: true,
+      chunks: ["index", "another", "text-editor"],
+      filename: "text-editor.html",
+    }),
   ],
 };
